bed518edcfb62589f172f17dd1af2b2509771352
davidajoyce <davidajoyce141@gmail.com>
2018-01-04 16:58:00 +0000

    implement descent gradient algorithm and cost function for data

--- linear_regres_methods.py
+++ linear_regres_methods.py
@@ -17,12 +17,47 @@ def create_data_table(path):
 		
 	data = pd.read_csv(path, header=None, names=['Population','Profit'])
 
+	#add a column of 1's to accomodate with the intercept term
 	data.insert(0, 'Ones', 1)
 
 	return data
 
 	
-#def gradient_descent_algo():
+def gradient_descent_algo(X, Y, theta, alpha, iters):
+
+	temp = np.matrix(np.zeros(theta.shape))
+
+	#size of theta matrix i.e parameters = 2
+	parameters = int(theta.ravel().shape[1])
+
+	cost = np.zeros(iters)
+	
+	for i in range(iters):
+		#calculate the error of predicted value y(theta)
+		error = (X*theta.T)-Y
+		
+		for j in range(parameters):
+			term = np.multiply(error, X[:,j])
+			temp[0,j] = theta[0,j] - ((alpha / len(X)) * np.sum(term))
+	
+		theta = temp
+		cost[i] = compute_the_cost(X,Y,theta)
+		#print "costing array"
+		#print cost[i]
+
+
+	return theta, cost
+
+def compute_the_cost(X,Y,theta):
+
+	inner =np.power(((X*theta.T)-Y),2)
+	
+	#least square function linear regression method
+	cost = np.sum(inner)/(2*len(X))
+
+	return cost
+
+	
 	
 
 #def Create_graph():
--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 import linear_regres_methods as lrm
 import os
+import numpy as np
 
 def main():
 
+	iters = 1000
+	alpha = 0.01
 
 	path = os.getcwd() + '/sampledata1.txt'
 	#setup data object where we can access all the relevant parameters for calculations
-
 	
 	#create table from txt file 
 	data_table = lrm.create_data_table(path)
 
-	data1 = lrm.Data(data_table)
+	data_matrix = lrm.Data(data_table)
 
-	print "test class"
-	print data1.X
+	theta = np.matrix(np.array([0,0]))
 
-	#data_matrix = lrm.Data(data_X, data_Y)	
+	#print "test class"
+	#print data_matrix.X	
 		
-	#Theta, Cost = lrm.gradient_descent_algo()
+	Theta, Cost = lrm.gradient_descent_algo(data_matrix.X, data_matrix.Y, theta, alpha, iters)
+
+	print "cost"
+	print Cost
 
 	#lrm.Create_graph()
 
 
 	
 main()
-
